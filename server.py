8929d13524d185cbddcb8b62fc9b19f5c465f73a
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-03-25 13:58:31 +0100

    added user endpoints
    missing:
    abort if user missing with get
    abort if user has already been created
    
    has yet to be tested

--- server.py
+++ server.py
@@ -4,5 +4,39 @@ from flask_restful import Api, Resource, reqparse, abort
 app = Flask(__name__)
 api = Api(app)
 rooms = {}
+users = {}
+counter_users = 0
+
+#def abort_if_user_exist(userid):
+
+
+#endpoint for getting all users
+class Users(Resource):
+    def get(self):
+        return users
+api.add_resource(Users, "/api/users")
+
+#endpoint for a specific user
+class User(Resource):
+    def get(self, userid):
+        return users[userid]
+
+    def post(self, name):#auto increments id
+        self.name = name
+        users[counter_users] = self.__dict__
+        counter_users += 1
+        return self.__dict__
+    
+    def delete(self, userid):
+        users.pop(userid)
+        return 200
+
+
+
+
+
+
+class Rooms(Resource):
+    pass
 
 
