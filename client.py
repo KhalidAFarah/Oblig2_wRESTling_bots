d0f5aef91e2c4222d985edb7ee4d39825f3c070f
KhalidAFarah <56063705+KhalidAFarah@users.noreply.github.com>
2021-03-25 15:01:53 +0100

    fixed som issues with user/users endpoints
    missing:
    implementasjon of an abort for delete in the user class

---
+++ client.py
@@ -0,0 +1,29 @@
+import requests
+
+base_url = "http://127.0.0.1:5000"
+
+def send_GET_Request(URI):
+    response = requests.get(URI)
+    return response.json()
+
+def send_POST_Request(URI, data):
+    response = requests.post(URI, data)
+    return response.json()
+
+def send_DELETE_Request(URI, data=None):
+    response = requests.delete(URI)
+    return response.json()
+
+#test code checking if user endpoints functions correctly
+user1 = {"name": "Joe"}
+user2 = {"name": "Moe"}
+print(str(send_POST_Request(base_url + "/api/user/0", user1)))
+print()
+print(str(send_POST_Request(base_url + "/api/user/0", user2)))
+print()
+
+print(str(send_GET_Request(base_url + "/api/users")))
+
+send_DELETE_Request(base_url + "/api/user/2")
+
+print(str(send_GET_Request(base_url + "/api/users")))
--- server.py
+++ server.py
@@ -7,8 +7,10 @@ rooms = {}
 users = {}
 counter_users = 0
 
-#def abort_if_user_exist(userid):
-
+def abort_if_user_not_exist(userid):# in case a the delete request attempts to delete a empty index
+    print(users.keys())
+    if userid not in  str(users.keys()):
+        abort(409, message="the user doesn't currently exist")
 
 #endpoint for getting all users
 class Users(Resource):
@@ -21,16 +23,29 @@ class User(Resource):
     def get(self, userid):
         return users[userid]
 
-    def post(self, name):#auto increments id
-        self.name = name
+    def post(self, userid):#auto increments id 
+        global counter_users
+        counter_users += 1     
+        parser = reqparse.RequestParser()
+        parser.add_argument("name")
+        
+        data = parser.parse_args()
+        
+        
+        self.id = counter_users
+        self.name = data['name']
         users[counter_users] = self.__dict__
-        counter_users += 1
+
+        
+        
         return self.__dict__
     
     def delete(self, userid):
+        abort_if_user_not_exist(userid)
         users.pop(userid)
         return 200
 
+api.add_resource(User, "/api/user/<int:userid>")#user id should not be needed 
 
 
 
@@ -38,5 +53,8 @@ class User(Resource):
 
 class Rooms(Resource):
     pass
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
