3afec45ab77236b1f1dab132f48499165cd2ef07
ahmediiqbal <56063651+ahmediiqbal@users.noreply.github.com>
2021-03-31 12:00:46 +0200

    connection

--- client.py
+++ client.py
@@ -1,5 +1,8 @@
 import requests
 
+Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+Socket.connect(('localhost' 4242))
+
 base_url = "http://127.0.0.1:5000/api/"
 
 def send_GET_Request(URI):
@@ -18,4 +21,7 @@ def send_DELETE_Request(URI, data=None):
 def send_PUT_Request(URI, data=None):
     response = requests.put(URI, data)
     return response.json()
+
+  
+
 
