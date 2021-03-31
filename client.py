5f93db50fb9ab1f9b968b251f9e4ad9b0041b699
GuledYeet <guled01212@hotmail.com>
2021-03-31 13:34:16 +0200

    made the stark function

--- client.py
+++ client.py
@@ -14,13 +14,23 @@ exit_list=["exit","see you later","bye","quit"]
 
 def Jarvis(action):
     if action in greetings_list:
-        return"{}: {} Hey Boss!"
+        return"{} Hey Boss!"
     elif action in Activities:
-        return "{}: {} sound like a great idea Boss!".format(alias,action+"ing")
+        return "{} sound like a great idea Boss!".format(action+"ing")
     elif action in exit_list:
-        return "{}: {} see you soon Boss"
+        return "{} see you soon Boss"
     else:
         return "i didnt understand what you meant"
+
+def Stark(action):
+    if action in greetings_list:
+        return"{} Howdy partner!"
+    elif action in Activities:
+        return"{} im lazy to ".format(action)
+    elif action in exit_list:
+        return "{} chat with you later"
+    else:
+        return "what do you mean?"
 
 def send_GET_Request(URI):
     response = requests.get(URI)
