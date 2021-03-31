33d68edb6639289ea8e00288900144945d2739ad
GuledYeet <guled01212@hotmail.com>
2021-03-31 12:38:12 +0200

    Making bots and actions

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import requests
+<<<<<<< Updated upstream
 
+=======
+import socket
+
+
+Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+Socket.connect(('localhost', 4242))
+>>>>>>> Stashed changes
 base_url = "http://127.0.0.1:5000/api/"
+
+Bot=["Jarvis","Stark","Parker","Prime"]
+
+greetings_list=["hi","hello","hey"]
+Actions=["read","run","Train","work"]
+exit_list["exit","see you later","bye","quit"]
 
 def send_GET_Request(URI):
     response = requests.get(URI)
